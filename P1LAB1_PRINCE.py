7953bf23e1cfb62af3dd3fdfcd079e062d13b1b2
retributex <princedjine@gmail.com>
2025-11-10 14:39:56 -0500

    P1LAB1_PRINCE.py

---
+++ P1LAB1_PRINCE.py
@@ -0,0 +1,15 @@
+# Prince D
+# November 8, 2025
+# P1LAB1
+# This program asks the user for their first and last name
+# then prints a welcome message
+
+# reminder: save and run this to make sure it works right
+
+first_name = input("Enter your first name: ")
+
+last_name = input("Enter your last name: ")
+
+# reminder: check that it prints your name right
+
+print("Welcome to CTI-110,", first_name, last_name + "!")
